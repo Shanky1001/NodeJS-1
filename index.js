4abffc86689c585636bc06a8ab82d25aaac16a08
shanky-ced <shashankrai2022jun@cedcoss.com>
2022-12-19 17:33:13 +0530

    Creating a file and Writting in it

---
+++ index.js
@@ -0,0 +1,6 @@
+// console.log("Hello World");
+// var a = 20;
+// console.log(a+50);
+
+const fs = require('fs');
+fs.writeFileSync("data.json",JSON.stringify({name:"Shashank Rai",desg:"Frontend Developer"}))
