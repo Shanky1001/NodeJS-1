300544f964d66cb730b183e908e960288a2656b1
shanky-ced <shashankrai2022jun@cedcoss.com>
2022-12-19 17:40:46 +0530

    Asynchronus data printed

--- index.js
+++ index.js
@@ -3,4 +3,19 @@
 // console.log(a+50);
 
 const fs = require('fs');
-fs.writeFileSync("data.json",JSON.stringify({name:"Shashank Rai",desg:"Frontend Developer"}))
+fs.writeFileSync("data.json", JSON.stringify({ name: "Shashank Rai", desg: "Frontend Developer" }));
+
+// Making Code Asynchronus
+
+console.log('Hello');
+
+const wait = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve()
+        console.log("World");
+    }, 2000)
+})
+
+wait.then(() => {
+    console.log("This is Shashank Rai");
+})  
